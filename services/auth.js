d486cfc246e0d5bf36ecd15a15b01145778752af
=Mtg_Dev <=mtg0987654321@gmail.com>
2020-04-11 22:44:59 +0200

    used promisfy

--- services/auth.js
+++ services/auth.js
@@ -1,38 +1,24 @@
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
-const generateAccessToken = dataToInclude => {
-  return new Promise((res, rej) => {
-    jwt.sign(
-      { ...dataToInclude },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "15m"
-      },
-      (err, token) => {
-        if (err) rej(err);
-        res(token);
-      }
-    );
+const generateAccessToken = async dataToInclude => {
+  //You can also use normal promises if you want
+  return await promisify(jwt.sign)(
+    { ...dataToInclude },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "15m"
+    }
+  );
+};
+
+const generateRefreshToken = async username => {
+  return await promisify(jwt.sign)({ username }, process.env.JWT_SECRET, {
+    expiresIn: "30d"
   });
 };
 
-const generateRefreshToken = username => {
-  return new Promise((res, rej) => {
-    jwt.sign(
-      { username },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "30d"
-      },
-      (err, token) => {
-        if (err) rej(err);
-        res(token);
-      }
-    );
-  });
-};
-
-const validateToken = token => {
+const validateToken = async token => {
   return new Promise((res, rej) => {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
