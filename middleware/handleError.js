e2d8f027f16dfc3291ac7ea25afb84c612391272
=Mtg_Dev <=mtg0987654321@gmail.com>
2020-05-06 02:37:50 +0200

    .

--- middleware/handleError.js
+++ middleware/handleError.js
@@ -1,6 +1,8 @@
 const { handleError, ErrorHandler } = require("../helpers/error");
 
 module.exports = (err, req, res, next) => {
+  console.log(err);
+
   if (!err.statusCode)
     return handleError(
       new ErrorHandler(400, "Invalid Data", "Please try again"),
